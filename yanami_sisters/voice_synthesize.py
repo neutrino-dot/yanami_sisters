5b9699f72ce74a4b3b01b416e48c23907ccce9ba
neutrino-dot <iz24919bd@isk.ed.jp>
2025-11-28 16:42:40 +0900

    Adjust output level in bpf filter function

--- yanami_sisters/voice_synthesize.py
+++ yanami_sisters/voice_synthesize.py
@@ -6,7 +6,7 @@ from scipy.signal import square,butter,sosfilt
 
 def _bpf(wave,formant):
   """bpfフィルタ関数"""
-  out = wave.copy()/2
+  out = wave.copy()/4
   for fc, bw in formant:
       low = fc - bw / 2
       high = fc + bw / 2
